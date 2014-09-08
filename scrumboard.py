bc869f33b785497a62c0b47369f1d035eccece7f
Maurice van der Pot <griffon26@kfk4ever.com>
2014-09-09 01:23:17 +0200

    Isolate individual contours and loop over rotations and square sizes
    Getting the part of the image that matches with the largest overlapping
    square is not implemented yet

--- scrumboard.py
+++ scrumboard.py
@@ -15,6 +15,28 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
+#
+# The purpose of this program is to monitor a physical scrum board using a
+# webcam, identify task notes and track how they move across the board over
+# time. It does not need to read text on the notes, it only needs to recognize
+# each note as distinct from all the other notes.
+#
+# It should use the information about tasks to interface with a digital
+# scrumboard such as the one provided by jira and keep task states in sync with
+# the physical board.
+#
+# In order to interact with the digital scrumboard the application must know
+# which physical note corresponds to which task in e.g. jira. I haven't chosen
+# yet how to deal with this. I think the easiest way from a user perspective
+# would be to ask for the JIRA task ID whenever the task is first seen on the
+# board (i.e. identified as a square that doesn't match with any previously
+# seen squares). An alternative could be a step at the start of the sprint to
+# set up (assisted by the user) the initial links between physical notes and
+# digital tasks.
+#
+
 import json
 import numpy as np
 import cv2
@@ -122,8 +144,8 @@ class Scrumboard():
         self.tasknotes.add(tasknote)
 
 class Square():
-    def __init__(self):
-        self.bitmap = None
+    def __init__(self, bitmap):
+        self.bitmap = bitmap
 
     def lookslike(otherthingwithbitmap):
         # self.bitmap ~ otherthingwithbitmap.bitmap
@@ -166,31 +188,36 @@ def findsquares(image):
     kernel = np.ones((3,3), np.uint8)
     color_only = cv2.morphologyEx(color_only, cv2.MORPH_CLOSE, kernel)
     color_only = cv2.morphologyEx(color_only, cv2.MORPH_OPEN, kernel)
+    print 'showing color_only'
     cv2.imshow(wndname, color_only)
-    waitforescape()
+    #waitforescape()
 
 
     colorless_only = 255 - color_only
     distance_to_color = cv2.distanceTransform(colorless_only, cv2.cv.CV_DIST_L2, 3)
 
     normdist = cv2.normalize(distance_to_color, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    print 'showing distance_to_color'
     cv2.imshow(wndname, normdist)
-    waitforescape()
+    #waitforescape()
 
     _, sure_bg = cv2.threshold(distance_to_color, 20, 1, cv2.THRESH_BINARY)
 
+    print 'showing sure_bg'
     cv2.imshow(wndname, sure_bg)
-    waitforescape()
+    #waitforescape()
 
     distance_to_colorless = cv2.distanceTransform(color_only, cv2.cv.CV_DIST_L2, 3)
 
     normdist = cv2.normalize(distance_to_colorless, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    print 'showing distance_to_colorless'
     cv2.imshow(wndname, normdist)
-    waitforescape()
+    #waitforescape()
 
     _, sure_fg = cv2.threshold(distance_to_colorless, 20, 1, cv2.THRESH_BINARY)
+    print 'showing sure_fg'
     cv2.imshow(wndname, sure_fg)
-    waitforescape()
+    #waitforescape()
 
     sure_fg8 = np.uint8(sure_fg)
 
@@ -199,32 +226,94 @@ def findsquares(image):
     markers = np.zeros(sure_fg.shape, np.int32)
     for i in xrange(n_comps):
         cv2.drawContours(markers, contours, i, i + 1, -1)
-    markers[sure_bg == 0] = n_comps + 1
+    markers[sure_bg == 1] = n_comps + 1
 
     normmarkers = cv2.normalize(markers, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    print 'showing markers before watershed'
     cv2.imshow(wndname, normmarkers)
     #waitforescape()
 
     cv2.watershed(hsv, markers)
 
     normmarkers = cv2.normalize(markers, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    print 'showing markers after watershed'
     cv2.imshow(wndname, normmarkers)
-    #waitforescape()
+    waitforescape()
 
-    component_contours = []
+    squares = []
     for i in xrange(n_comps):
         singlecomponent = cv2.inRange(markers, i, i)
-        contours = cv2.findContours(singlecomponent, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        contours, hierarchy = cv2.findContours(singlecomponent.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        norm = cv2.normalize(singlecomponent, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+        print 'showing single component %d' % i
+        cv2.imshow(wndname, norm)
+        #waitforescape()
 
         if len(contours) != 0:
             if len(contours) > 1:
                 # TODO: check if ignoring is the right thing to do
-                raise RuntimeError("More than one (%d) external contour found for component %d. Not sure what to do with this. Ignoring component for now." % (len(contours), i))
+                raise RuntimeError ("More than one (%d) external contour found for component %d. Not sure what to do with this. Ignoring component for now." % (len(contours), i))
             else:
-                component_contours.append(contours[0])
+                contour = contours[0]
 
+                center, radius = cv2.minEnclosingCircle(contour)
+                size = (int(radius) * 2, int(radius) * 2)
+
+                singlecontour = cv2.getRectSubPix(singlecomponent, size, center)
+                imagecutout = cv2.getRectSubPix(image, size, center)
+
+                square_bitmap = find_largest_overlapping_square(singlecontour, imagecutout)
+                squares.append(Square(square_bitmap))
 
     return []
+
+
+def find_largest_overlapping_square(singlecontour, imagecutout):
+
+    overallMax = 0
+
+    newcenter = (singlecontour.shape[0] / 2, singlecontour.shape[1] / 2)
+
+    for angle in xrange(-16,17):
+        rotation = cv2.getRotationMatrix2D(newcenter, angle, 1.0)
+        rotatedcontour = cv2.warpAffine(singlecontour, rotation, singlecontour.shape)
+
+        cv2.imshow(wndname, rotatedcontour)
+        #waitforescape()
+
+        offsetcontour = rotatedcontour.astype(np.float32) - 160
+
+        for kernel_size in xrange(50,70):
+            boxfiltered = cv2.boxFilter(offsetcontour, -1, (kernel_size, kernel_size), None, (0,0), False)
+
+            norm = cv2.normalize(boxfiltered, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+            cv2.imshow(wndname, norm)
+            #waitforescape()
+
+
+            mask = np.zeros(boxfiltered.shape, np.uint8)
+            mask[0:boxfiltered.shape[0] - kernel_size + 1, 0:boxfiltered.shape[1] - kernel_size + 1] = 1
+
+            minVal, maxVal, _, maxLoc = cv2.minMaxLoc(boxfiltered, mask)
+
+            if maxVal > overallMax:
+                overallMax = maxVal
+                overallMaxLoc = maxLoc
+                overallMaxAngle = angle
+                overallMaxKernelSize = kernel_size
+
+    bitmap = None
+    #if overallMax > 0:
+    #    rotation = cv2.getRotationMatrix2D(newcenter, overallMaxAngle, 1.0)
+
+    #    rotatedimagecutout = cv2.warpAffine(imagecutout, rotation, imagecutout.shape)
+
+    #    bitmap = rotatedimagecutout[overallMaxLoc.x:overallMaxLoc.x + overallMaxKernelSize, overallMaxLoc.y:overallMaxLoc.y + overallMaxKernelSize]
+
+    return bitmap
+
 
 if __name__ == "__main__":
     cv2.namedWindow( wndname, 1 );
