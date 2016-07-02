26add8061762dc5b09551469dafba33907769ba7
Maurice van der Pot <griffon26@kfk4ever.com>
2016-07-02 23:30:41 +0200

    findnotes: first version that works reasonably well

--- findnotes.py
+++ findnotes.py
@@ -25,6 +25,7 @@ import os
 import sys
 
 import common
+import webcam
 
 from PyQt5 import QtWidgets
 
@@ -56,19 +57,62 @@ def determine_average_colors(image, circleKernel):
     kernelSize = cv2.countNonZero(circleKernel)
 
     averages = cv2.filter2D(image, -1, circleKernel.astype(np.float32) / kernelSize)
-    common.qimshow(averages)
     return averages
+
+def remove_color_cast(image):
+    bgr_planes = cv2.split(image)
+
+    backgroundpos = calibrationdata['background']
+    average_bgr = image[backgroundpos[1]][backgroundpos[0]]
+    print 'bgr of scrumboard background:', average_bgr
+
+    for i in xrange(3):
+        thresh = average_bgr[i]
+        plane = bgr_planes[i]
+        print "Showing plane"
+        #qimshow(plane);
+
+        retval, mask = cv2.threshold(plane, thresh, 255, cv2.THRESH_BINARY);
+        print "Showing mask"
+        #qimshow(mask)
+
+        highvalues = (plane - thresh) * (128.0 / (255 - thresh)) + 128;
+        highvalues = highvalues.astype(np.uint8)
+
+        highvalues_masked = cv2.bitwise_and(highvalues, mask)
+        print "Showing scaled high values"
+        #qimshow(highvalues_masked)
+
+        mask = 255 - mask;
+        lowvalues = cv2.bitwise_and(plane, mask)
+        print "Showing low values"
+        #qimshow(lowvalues)
+
+        lowvalues = lowvalues * 128.0 / thresh
+        lowvalues = lowvalues.astype(np.uint8)
+        print "Showing scaled low values"
+        #qimshow(lowvalues)
+
+        bgr_planes[i] = lowvalues + highvalues_masked
+        print "Showing scaled plane"
+        #qimshow(bgr_planes[i])
+
+    correctedimage = cv2.merge(bgr_planes)
+    correctedimage = correctedimage.astype(np.uint8)
+    print "Showing corrected image"
+    #qimshow(correctedimage)
+
+    return correctedimage
 
 def findnotes(image):
     #averages = determine_average_colors(image)
 
     common.qimshow(image)
 
-    median = cv2.medianBlur(image, common.NOTE_SIZE - 1)
+    median = cv2.medianBlur(image, 11)
     common.qimshow(median)
 
     padded = np.pad(image, ((common.NOTE_SIZE / 2, common.NOTE_SIZE / 2), (common.NOTE_SIZE / 2, common.NOTE_SIZE / 2), (0,0)), 'constant')
-    common.qimshow(padded)
 
     circle = createCircularKernel()
 
@@ -102,43 +146,55 @@ def findnotes(image):
                 deviation += squaredsum / 3
 
 
-    
     deviation_norm = np.zeros(deviation.shape, dtype=np.float32)
     deviation_norm = cv2.normalize(deviation, deviation_norm, 0, 1, cv2.NORM_MINMAX)
     deviation_norm = 1.0 - deviation_norm
 
-    #deviation_uint = np.uint8(deviation_norm * 255)
-    #peaks1 = cv2.adaptiveThreshold(deviation_uint, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, common.NOTE_SIZE / 2, -3)
-    #peaks2 = cv2.adaptiveThreshold(deviation_uint, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, common.NOTE_SIZE / 2, -5)
-    #peaks3 = cv2.adaptiveThreshold(deviation_uint, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, common.NOTE_SIZE / 2, -7)
-    #common.qimshow([deviation_uint, peaks1, peaks2, peaks3])
 
-    kernel = np.ones((common.NOTE_SIZE - 9, common.NOTE_SIZE - 9), np.uint8)
+    kernel = np.ones((common.NOTE_SIZE / 2, common.NOTE_SIZE / 2), np.uint8)
     deviation_dil = cv2.dilate(deviation_norm, kernel)
     deviation_sub = deviation_norm - deviation_dil
     minval, maxval, _, _ = cv2.minMaxLoc(deviation_sub)
     print 'min was %f, max was %f' % (minval, maxval)
 
-    norm = np.zeros(deviation_dil.shape, dtype=np.float32)
-    norm = cv2.normalize(deviation_dil, norm, 0, 1, cv2.NORM_MINMAX)
-    common.qimshow(norm)
+    deviation_subclip = (deviation_sub > -0.0001).astype(np.float32)
 
-    deviation_sub2 = (deviation_norm < deviation_dil).astype(np.float32)
+    hsv = cv2.cvtColor( median, cv2.COLOR_BGR2HSV )
+    _, saturation, _ = cv2.split(hsv)
+    color_only = cv2.inRange(saturation, 25, 255)
 
-    common.qimshow([deviation_norm, deviation_dil, deviation_sub, deviation_sub2])
-    peaks = (deviation_sub == 0).astype(np.float32)
+    deviation_subclip2 = cv2.inRange(deviation_subclip, 0.5, 1.0)
+    deviation_subclip2[color_only == 0] = 0
 
+    _, contours, _ = cv2.findContours(deviation_subclip2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    annotatedimage = image.copy()
+    for contour in contours:
+        center, _ = cv2.minEnclosingCircle(contour)
+        cv2.circle(annotatedimage, tuple(int(f) for f in center), common.NOTE_SIZE / 2, (0,0,255))
+        print 'contour is at %s' % (center,)
+
+    common.qimshow([ [deviation_norm],
+                     [deviation_dil],
+                     [deviation_sub],
+                     [deviation_subclip],
+                     [color_only],
+                     [deviation_subclip2],
+                     [annotatedimage]
+                   ])
 
     return []
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
-    image = cv2.imread('board1.png')
+    #image = cv2.imread('board.png')
+    image = webcam.grab()
 
     with open('calibrationdata.json', 'rb') as f:
         calibrationdata = json.loads('\n'.join(f.readlines()))
-    image, _ = common.correct_perspective(image, calibrationdata, False)
+    image, _ = common.correct_perspective(remove_color_cast(image), calibrationdata, False)
 
-    notes = findnotes(image[:200, 100:-100])
+    #notes = findnotes(image[600:800,200:500])
+    notes = findnotes(image)
 
